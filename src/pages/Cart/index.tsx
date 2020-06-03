4a0d573687ddea978b22c0ffd419e545ef9076f6
ricardojunior02 <ricardojunior199502@gmail.com>
2020-06-03 18:39:16 -0300

    Lista itens no carrinho

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -27,35 +27,35 @@ import { useCart } from '../../hooks/cart';
 
 import formatValue from '../../utils/formatValue';
 
-interface Product {
-  id: string;
-  title: string;
-  image_url: string;
-  price: number;
-  quantity: number;
-}
-
 const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productTotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + productTotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productTotal = product.quantity;
 
-    return 0;
+      return accumulator + productTotal;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
@@ -68,7 +68,7 @@ const Cart: React.FC = () => {
           ListFooterComponentStyle={{
             height: 80,
           }}
-          renderItem={({ item }: { item: Product }) => (
+          renderItem={({ item }) => (
             <Product>
               <ProductImage source={{ uri: item.image_url }} />
               <ProductTitleContainer>
